abd33d8bc0b6880effc62188aadbf81e03cd3fbb
luke-h1 <luke.howsam@yahoo.com>
2020-10-31 15:08:20 +0000

    CRA cleanup, add dependencies

---
+++ README.md
@@ -0,0 +1,4 @@
+## Netflix Clone: 
+
+
+ * tutorial from https://www.youtube.com/watch?v=x_EEwGe-a9o 
---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React,{Fragment} from 'react';
+
+
+const App = () =>  {
+  return (
+    <Fragment>
+      <h1>Hello</h1>
+    </Fragment>
+  );
+}
+
+export default App;
