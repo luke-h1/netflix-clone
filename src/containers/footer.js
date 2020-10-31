4f5e9c549c94628dd9abbc85cc55c39168da7536
luke-h1 <luke.howsam@yahoo.com>
2020-10-31 17:20:05 +0000

    finish Footer components

--- src/containers/footer.js
+++ src/containers/footer.js
@@ -1,5 +1,6 @@
 /* eslint-disable react/jsx-filename-extension */
 /* eslint-disable import/prefer-default-export */
+/* eslint-disable */
 import React from "react";
 import { Footer } from "../components";
 
@@ -7,6 +8,34 @@ export function FooterContainer() {
   return (
     <Footer>
       <Footer.Title>Questions? Contact Us</Footer.Title>
+      <Footer.Break />
+      <Footer.Row>
+        <Footer.Column>
+          <Footer.Link href="#">FAQ</Footer.Link>
+          <Footer.Link href="#">Investor relations</Footer.Link>
+          <Footer.Link href="#">Ways to watch</Footer.Link>
+          <Footer.Link href="#">Corporate information</Footer.Link>
+          <Footer.Link href="#">Netflix Originals</Footer.Link>
+        </Footer.Column>
+        <Footer.Column>
+          <Footer.Link href="#">Help Centre</Footer.Link>
+          <Footer.Link href="#">Jobs</Footer.Link>
+          <Footer.Link href="#">Terms of use</Footer.Link>
+          <Footer.Link href="#">Contact Us</Footer.Link>
+        </Footer.Column>
+        <Footer.Column>
+          <Footer.Link href="#">Account</Footer.Link>
+          <Footer.Link href="#">Redeem Gift Cards</Footer.Link>
+          <Footer.Link href="#">privacy</Footer.Link>
+          <Footer.Link href="#">Speed Test</Footer.Link>
+        </Footer.Column>
+        <Footer.Column>
+          <Footer.Link href="#">Media Centre</Footer.Link>
+          <Footer.Link href="#">Buy Gift Cards</Footer.Link>
+          <Footer.Link href="#">Cookie preferences</Footer.Link>
+          <Footer.Link href="#">Legal Notices</Footer.Link>
+        </Footer.Column>
+      </Footer.Row>
     </Footer>
   );
 }
